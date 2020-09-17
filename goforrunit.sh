794cb33a2b9bf689da84af29e094600cde751053
driver <alex.tisserand@hotmail.com>
2020-09-17 16:56:08 +0200

    Commit de reprise du projet

--- goforrunit.sh
+++ goforrunit.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # To create and run the modbus container :)
+docker create 
 docker run (-it) --name libmodbuscontainer libmodbusimage:0.1.0
+docker run -it --name libmodbuscontainer libmodbusimage:0.1.0
 
 # To copy a file form a container to the host local directory
-docker cp libmodbuscontainer:'/tmp/imafile' ./  
+docker cp libmodbuscontainer:'/libmodbus_project/driverdev.db' ./
--- src/driveralexsoft.c
+++ src/driveralexsoft.c
@@ -6,6 +6,11 @@
 #include <stdlib.h>
 #include <errno.h>
 
+
+
+#include <time.h>
+#include <sys/time.h>  
+
 #include <modbus.h>
 
 #include <sqlite3.h>
@@ -13,8 +18,41 @@
 #define ADDRESS_START   1450
 #define ADDRESS_END     1500
 
+
+
+long long int drivetime(void)
+{
+    /* Example of timestamp in microsecond. */
+    struct timeval timer_usec; 
+    long long int timestamp_usec; /* timestamp in microsecond */
+    if (!gettimeofday(&timer_usec, NULL)) 
+    {
+        timestamp_usec = ((long long int) timer_usec.tv_sec) * 1000000ll + (long long int) timer_usec.tv_usec;
+    }
+    else 
+    {
+        timestamp_usec = -1;
+    }
+    // printf("%lld microseconds since epoch\n", timestamp_usec);
+    
+    
+    return timestamp_usec;
+}
+
+char * stringcreate(char *a,char *b )
+{
+    char *tmp = (char * ) malloc(1+ strlen(a)+strlen(b));
+    strcpy(tmp,a);
+    strcat(tmp,b);
+    return tmp;
+}
+
 int main(void)
 {
+    char outputtime[1024];
+
+    sprintf(outputtime,"%lld",drivetime());
+
     char *err_msg = 0;
     printf("SQLite3 version: %s\n", sqlite3_libversion()); 
 
@@ -48,11 +86,69 @@ int main(void)
         printf("%s\n", sqlite3_column_text(res, 0));
     }
 
-    char *sql = "CREATE TABLE TimeRef(Idscrap INT, TableTime TEXT);"
-                "INSERT INTO TimeRef VALUES(0, '3/9/2020  1:00:00 AM');"
-                "INSERT INTO TimeRef VALUES(0, '3/9/2020  1:00:00 AM');";
+    char *sql = "CREATE TABLE TimeRef(Idscrap INT, TableTimestamp TEXT);";
+                "INSERT INTO TimeRef VALUES(0,'1523115');";
+               
+    char *timerefinsert = "INSERT INTO TimeRef VALUES(1,'";
+    char *endins = "');";
+    char *concat1 = (char * ) malloc(1+ strlen(timerefinsert)+strlen(outputtime));
+    
+    /*strcpy(concat1,timerefinsert);
+    strcat(concat1,outputtime);
+    printf("\nString 1:\n%s\n",concat1);*/
+    
+    
+    concat1 = stringcreate(timerefinsert,outputtime);
 
-    rcd = sqlite3_exec(db, sql, 0, 0, &err_msg);
+    
+    //char *concat2 = (char * ) malloc(1+ strlen(concat1)+strlen(endins));
+    char *concat2 ;
+    concat2 = stringcreate(stringcreate(timerefinsert,outputtime),endins);
+
+    printf("\nString 2:\n%s\n",concat2);
+
+    /*
+    printf("Before");
+    printf("%s",concat2);
+    printf("After");
+    */
+
+    /*Faire une fonction d'ajout des 2 parm vers un tableau ( la requette sql )*/
+
+
+    /* Creation de la table frogien */ 
+    char *newrowscraptable = "CREATE TABLE tab_";
+    char *rawtabletimereftemp = (char * ) malloc(1+ strlen(newrowscraptable)+strlen(outputtime));
+    strcpy(rawtabletimereftemp,newrowscraptable);
+    strcat(rawtabletimereftemp,outputtime);
+
+    char *inside1 = "(Modbusaddr INT, Value INT);";
+    char *rawcreatfinal = (char * ) malloc(1+ strlen(rawtabletimereftemp)+strlen(inside1));
+    strcpy(rawcreatfinal,rawtabletimereftemp);
+    strcat(rawcreatfinal,inside1);
+    /*printf("Before");
+    printf("%s",rawcreatfinal);
+    printf("After");*/
+
+
+    char *insertforgeintable = "INSERT INTO tab_";
+    char *insertforgeintablertmp = (char * ) malloc(1+ strlen(insertforgeintable)+strlen(inside1));
+    strcpy(insertforgeintablertmp,insertforgeintable);
+    strcat(insertforgeintablertmp,outputtime);
+
+    
+
+    printf("Before");
+    printf("%s",insertforgeintablertmp);
+    printf("After");
+    printf("Beforefunct");
+    printf("%s",stringcreate(insertforgeintable,outputtime));
+    printf("After");
+
+
+
+
+    rcd = sqlite3_exec(db, rawcreatfinal, 0, 0, &err_msg);
     if (rcd != SQLITE_OK )
     {
         fprintf(stderr, "SQL error: %s\n", err_msg);
